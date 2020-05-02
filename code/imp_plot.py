c4491d2b0a648da9199eb15255334534162fa74e
Tyler Ashoff <ashofftyler@gmail.com>
2020-05-02 12:30:39 -0700

    draft

---
+++ code/imp_plot.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import xgboost as xgb
+
+imp = pickle.load(open('./code/xgboost_imp.p', 'rb'))
+
+labels = np.flip(['Journal', 'Date', 'Avg Abs 6M', 'Abs Close Count', 'Avg Body', 'Avg Abs', 'Body Close Count', 'Avg Body 6M'])
+
+plot = xgb.plot_importance(imp)
+plot.set_yticklabels(labels)
+plot.set_xticks([])
+plot.grid(False)
+plot
+plt.box(False)
+plt.ylabel('')
+plt.xlabel('F score', position=(0.35,0))
+plt.suptitle('XGBoost Feature Importance', fontsize=12)
+plt.title('Variational Autoencoder Embedding                            .',y=0.92, fontsize=10)
+plt.tight_layout()
+plt.savefig('./Figures/xgboost_feature_importance.png')
+plt.show()
+
